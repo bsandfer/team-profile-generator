6ad85c546c5312a2edf4628fba37f6c22d27bbfc
BSandfer <bsandfer@gmail.com>
2022-04-10 21:58:19 -0700

    finished

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const fse = require('fs-extra');
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-const { finished } = require("stream");
+// const { finished } = require("stream");
 
 
 // Write code to use inquirer to gather information about the development team members,
@@ -38,7 +39,14 @@ const { finished } = require("stream");
 let employeeArray = []
 
 const finished = () => {
-    
+    let myTeamHtml = render(employeeArray)
+    fse.outputFile('output/team.html', myTeamHtml)
+   .then(() => {
+       console.log('The file has been saved!');
+   })
+   .catch(err => {
+       console.error(err)
+   });
 }
 
 const questContinue = () => {
